6fef1c55d3a6c70c542d745823b874ec2df63577
Alyssa Rosenzweig <alyssa@collabora.com>
2021-04-16 18:36:06 -0400

    agx: Add bitwise operations
    This get translated to bitop with the corresponding truth table with
    some builder syntax sugar.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@collabora.com>

--- src/asahi/compiler/agx_compile.c
+++ src/asahi/compiler/agx_compile.c
@@ -247,6 +247,14 @@ agx_emit_alu(agx_builder *b, nir_alu_instr *instr)
    UNOP(fddy_coarse, dfdy);
    UNOP(fddy_fine, dfdy);
 
+   UNOP(mov, mov);
+   UNOP(u2u16, mov);
+   UNOP(u2u32, mov);
+   UNOP(inot, not);
+   BINOP(iand, and);
+   BINOP(ior, or);
+   BINOP(ixor, xor);
+
    case nir_op_fsub: return agx_fadd_to(b, dst, s0, agx_neg(s1));
    case nir_op_fabs: return agx_fmov_to(b, dst, agx_abs(s0));
    case nir_op_fneg: return agx_fmov_to(b, dst, agx_neg(s0));
